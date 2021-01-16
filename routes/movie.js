f63c7ff9954ec8b761945bf9626a6413e75ca294
ezgiselinkilic <ezgiselin00@gmail.com>
2021-01-16 17:30:49 +0300

    /api/movies [GET] edit

--- routes/movie.js
+++ routes/movie.js
@@ -6,7 +6,20 @@ var router = express.Router();
 const Movie = require('../models/Movie');
 
 router.get('/', (req, res) => {
-  const promise = Movie.find({});
+  const promise = Movie.aggregate([
+
+    {
+      $lookup:{
+        from:'directors',
+        localField: 'director_id',
+        foreignField:'_id',
+        as:'director'
+      }
+    },
+    {
+      $unwind:'$director'
+    }
+  ]);
   promise.then((data) => {
     res.json(data);
   }).catch((err) => {
